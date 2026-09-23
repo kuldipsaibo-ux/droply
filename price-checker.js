35d76441fc1a972a459e70e2c7cbddfbd699ecdc
kuldipsaibo-ux <kuldipsaibo@gmail.com>
2026-09-24 00:06:10 +0530

    Update price-checker.js

--- price-checker.js
+++ price-checker.js
@@ -1,27 +1,35 @@
-function checkPrice(alert, currentPrice) {
-  const targetPrice = Number(alert.targetPrice);
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
 
-  if (currentPrice <= targetPrice) {
-    return {
-      triggered: true,
-      message: `🔥 Price dropped to ₹${currentPrice}! Target: ₹${targetPrice}`
-    };
-  }
+import {
+  getFirestore,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
 
-  return {
-    triggered: false,
-    message: `Still watching. Current: ₹${currentPrice} | Target: ₹${targetPrice}`
-  };
-}
-
-// TEST ALERT
-const alert = {
-  targetPrice: 2200
+const firebaseConfig = {
+  apiKey: "AIzaSyAuQvRtusTqOmb_tA0xQm12VJPQQYnvzfA",
+  authDomain: "droply-fa6f6.firebaseapp.com",
+  projectId: "droply-fa6f6",
+  storageBucket: "droply-fa6f6.firebasestorage.app",
+  messagingSenderId: "993632750410",
+  appId: "1:993632750410:web:373d3d6399ad705d874fb3",
+  measurementId: "G-MQ7JRR824Q"
 };
 
-// Fake current price for testing
-const currentPrice = 1999;
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-const result = checkPrice(alert, currentPrice);
+async function loadAlerts() {
+  const snapshot = await getDocs(collection(db, "priceAlerts"));
 
-console.log(result.message);
+  snapshot.forEach((doc) => {
+    const alert = doc.data();
+
+    console.log("Watching:", alert.productUrl);
+    console.log("Target price:", alert.targetPrice);
+  });
+}
+
+loadAlerts();
